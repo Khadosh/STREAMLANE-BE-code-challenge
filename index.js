1bb33854090ce5694f90771679fb83429df5fd43
Ariel Epelman <arielepelman@gmail.com>
2025-04-24 15:09:37 -0300

    feat: add 2 new endpoints

--- index.js
+++ index.js
@@ -17,7 +17,44 @@ app.get("/users/:id", async (req, res) => {
   const user = await fetch(`${USERS_URL}/${req.params.id}`).then(res => res.json());
   res.send(user);
 });
+async function getUsers(){
+  return  fetch(USERS_URL).then(res => res.json());
 
+}
+async function getPosts(users){
+  let request =[];
+  for(let i=0; i<users.length; i++){
+
+    
+    request.push(fetch(`${POSTS_URL}?userId=${users[i].id}`));
+  }
+  const posts=  await Promise.all(request)
+  return posts;
+
+}
+ function getReport(users, posts){
+  let userPost =[];
+
+  for(let j=0; j<posts.length;j++){
+    userPost.push({
+      name:users[j].name,
+      email: users[j].email,
+      postCount:posts.length
+    })
+  }
+}
+app.get('/report', async (req, res)=>{
+    let users = await getUsers();
+    console.log("🚀 ~ app.get ~ users:", users)
+    let posts = await getPosts(users);
+    console.log("🚀 ~ app.get ~ posts:", posts)
+    
+  let result =getReport(users, posts);
+  console.log("🚀 ~ app.get ~ result:", result)
+  
+  res.send();
+
+})
 app.listen(port, () => {
   console.log(`Sandbox listening  http://localhost:${port}`);
 });
